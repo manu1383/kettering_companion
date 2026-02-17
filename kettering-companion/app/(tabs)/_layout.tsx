057ec063902cbae7298c1bb750d59f031e39612d
William Erickson <will.erickson524@gmail.com>
2026-02-16 21:23:00 -0500

    Login Changes + Logout button

--- kettering-companion/app/(tabs)/_layout.tsx
+++ kettering-companion/app/(tabs)/_layout.tsx
@@ -20,8 +20,8 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          title: 'Logout',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="arrow.right.square" color={color} />,
         }}
       />
       <Tabs.Screen
